a580857ca7add333e862bcc6c5ea4b21948b857f
VidyasagarPandikasahala <vidya.sagar.six@gmail.com>
2023-05-28 12:40:57 +0530

    made changes to app.js file

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello ammada kutty !!!!!!!!!!!</p>
+        <p>Hello Folks !!!!!!!!!!!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
